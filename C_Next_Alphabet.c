3addc5ce4f330b827a4c3ace89a90711cea51ded
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2025-04-15 15:59:42 +0600

    Solved first problem of module 6.5

---
+++ C_Next_Alphabet.c
@@ -0,0 +1,19 @@
+// #include <stdio.h>
+
+// int main()
+// {
+//     char alphabet;
+
+//     scanf("%c", &alphabet);
+
+//     if (alphabet == 'z')
+//     {
+//         printf("%c", 97);
+//     }
+//     else
+//     {
+//         printf("%c", alphabet + 1);
+//     }
+
+//     return 0;
+// }
